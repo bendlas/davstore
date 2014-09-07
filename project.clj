df787f6a440baf23934584bfa48c616b43877588
Herwig Hochleitner <herwig@bendlas.net>
2014-09-08 01:14:39 +0200

    More updated deps

--- project.clj
+++ project.clj
@@ -5,15 +5,15 @@
     - A connector to expose files over webdav"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["bendlas-nexus" {:url "http://nexus.bendlas.net/content/groups/public"
+                                   :username "fetch" :password :gpg}]]
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                  [webnf/base "0.0.12-SNAPSHOT"]
-                 [webnf/handler "0.0.11"]
+                 [webnf/handler "0.0.12-SNAPSHOT"]
                  [webnf/datomic "0.1.0-SNAPSHOT"]
-                 [webnf/enlive.clj "0.0.3"]
-                 [webnf.deps/contrib "0.0.3"]
-                 [org.clojure/data.xml "9999-bendlas-SNAPSHOT"]
-                 [org.clojure/core.typed "0.2.66"]
-                 [de.kotka/lazymap "3.1.1"]
+                 [webnf/enlive.clj "0.0.4-SNAPSHOT"]
+                 [webnf.deps/universe "0.0.1-SNAPSHOT"]
+                 [net.bendlas/data.xml "1.0.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.3.0"]
                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.11"]]
